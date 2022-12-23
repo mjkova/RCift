1c0892ac9d2cfa9e2d71c6fd752d30c804d60963
mjkova <78625324+mjkova@users.noreply.github.com>
2022-12-22 19:32:43 -0700

    Update app.rs

--- app.rs
+++ app.rs
@@ -1,5 +1,5 @@
 use std::env::args;
-use std::fs;
+mod file;
 
 fn main() { 
     let args : Vec<String> = args().collect();
@@ -26,25 +26,5 @@ fn main() {
         return;
     }
 
-    println!("{}", get_line(0, file_path));
-}
-
-fn get_line(line: i32, path: String) -> String {
-    let split_char : String = "\n".to_string();
-    
-    let contents = fs::read_to_string(path)
-        .expect("File is not readable, try checking permissions");
-
-    let iter_for_lines = contents.split(&split_char);
-
-    let mut counter : i32 = 0;
-
-    for item in iter_for_lines {
-        if counter == line {
-            return item.parse::<String>().unwrap();
-        }
-        counter += 1;
-    }
-    
-    return "Could not find line".to_string();
+    println!("{}", file::get_line(0, file_path));
 }
