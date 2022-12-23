5acf77ee203f888518c2e8a67fe5ddf91b4022b7
mjkova <78625324+mjkova@users.noreply.github.com>
2022-12-22 23:30:17 -0700

    Update execute.rs

--- execute.rs
+++ execute.rs
@@ -1,4 +1,24 @@
-pub fn execute(line: String) {
+use std::collections::HashMap;
+
+pub fn set_ints(line: String) -> HashMap<String, i32> {
+    let mut line_array : Vec<String> = line.split(" ").map(|s| s.to_string()).collect();
+
+    let mut integers: HashMap<String, i32> = HashMap::new();
+    
+    let mut chars = line_array[3].chars();
+    chars.next_back();
+    line_array[3] = chars.as_str().to_string();
+    
+    if &line_array[0] == &"int".to_string() {
+        integers.insert(String::from(&line_array[1]), line_array[3].parse::<i32>().unwrap());
+    }
+
+    integers
+}
+
+pub fn get_int(line: String, integers: HashMap<String, i32>) -> i32 {
+    return integers.get(&line.clone()).unwrap_or(&0).clone();
+} fn execute(line: String) {
     let line_array : Vec<String> = line.split(" ").map(|s| s.to_string()).collect();
 
     if line_array[2] == "=" {
