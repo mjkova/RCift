8ee8502a79fd587683e369254702ef68398a9e88
mjkova <78625324+mjkova@users.noreply.github.com>
2022-12-23 18:36:38 -0700

    Update check_null.rs

--- check_null.rs
+++ check_null.rs
@@ -1,7 +1,15 @@
 static INT_NULL : i32 = i32::MIN;
+static STRING_NULL : &str = "Cift : String : None";
 
 pub fn is_int_null(int: i32) -> bool {
-    if int == INT_NULL {
+    if &int == &INT_NULL {
+        return true;
+    }
+    return false;
+}
+
+pub fn is_string_null(string: String) -> bool {
+    if &string == &STRING_NULL {
         return true;
     }
     return false;
