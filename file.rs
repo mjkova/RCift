02fac4c488ecf0f4deeae325c9876960ae4e4d79
mjkova <78625324+mjkova@users.noreply.github.com>
2022-12-23 18:35:19 -0700

    Update file.rs

--- file.rs
+++ file.rs
@@ -17,5 +17,22 @@ pub fn get_line(line: i32, path: String) -> String {
         counter += 1;
     }
     
-    return "Could not find line".to_string();
+    return "Cift : String : None".to_string();
+}
+
+pub fn get_lines_count(path: String) -> i32 {
+    let split_char : String = "\n".to_string();
+    
+    let contents = fs::read_to_string(path)
+        .expect("File is not readable, try checking permissions");
+
+    let iter_for_lines = contents.split(&split_char);
+
+    let mut counter : i32 = 0;
+
+    for _item in iter_for_lines {
+        counter += 1;
+    } 
+
+    return counter;
 }
