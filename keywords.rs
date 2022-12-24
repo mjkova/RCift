c9f8df04c82c3a44fea618c838871597f661ec02
mjkova <78625324+mjkova@users.noreply.github.com>
2022-12-23 21:23:34 -0700

    Update keywords.rs

--- keywords.rs
+++ keywords.rs
@@ -9,6 +9,14 @@ pub fn get_and_execute(line: String, hash: HashMap<String, i32>) {
         if execute::is_int_variable(slice.clone().to_string(), hash.clone()) {
             print(execute::get_int(slice.clone().to_string(), hash.clone()).to_string());
         }
+        if slice == '"' {
+            let start_bytes = line.find('(').unwrap(); 
+            
+            let end_bytes = line.find(')').unwrap();
+            let result = &line[start_bytes+2..end_bytes-1];
+
+            print(result.to_string());
+        }
     }
 }
 
