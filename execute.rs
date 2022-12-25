561bab045cf7ea1b3110facc56bc00c0cc74dec7
mjkova <78625324+mjkova@users.noreply.github.com>
2022-12-24 19:06:48 -0700

    Update execute.rs

--- execute.rs
+++ execute.rs
@@ -4,37 +4,20 @@ use crate::file;
 use crate::check_null;
 
 // BEGIN INTEGER SECTION
-pub fn set_all_ints(file_name: String) -> HashMap<String, i32> {
-    let mut map : HashMap<String, i32> = HashMap::new();
-
-    let lines : i32 = file::get_lines_count(file_name.clone());
-
-    for i in 0..lines-1 {
-        if !check_null::is_int_null(i) {
-            let res : String = file::get_line(i, file_name.clone());
-            if !check_null::is_string_null(res.clone()) {
-                map.extend(set_ints(res));
-            }
-        }
-    }
-
-    map
-}
-
-pub fn set_ints(line: String) -> HashMap<String, i32> {
+pub fn set_int(line: String) -> HashMap<String, i32> {
     let mut line_array : Vec<String> = line.split(" ").map(|s| s.to_string()).collect();
 
     let mut integers: HashMap<String, i32> = HashMap::new();
     
-    let mut chars = line_array[3].chars();
-    chars.next_back();
-    line_array[3] = chars.as_str().to_string();
-
     if &line_array[0] != &"int".to_string() {
         return integers;
     }
     
-    if line_array[3].contains(".") {
+    let mut chars = line_array[3].chars();
+    chars.next_back();
+    line_array[3] = chars.as_str().to_string();
+    
+    if line_array[3].contains(".") || check_null::is_int_null(line_array[3].clone().parse::<i32>().unwrap()) {
         println!("Integer {}, is not an integer", line_array[1]);
         return integers;
     }
@@ -64,35 +47,18 @@ pub fn is_int_variable(name: String, hash: HashMap<String, i32>) -> bool {
 // END INTEGER SECTION
 
 // BEGIN STRING SECTION
-pub fn set_all_strings(file_name: String) -> HashMap<String, String> {
-    let mut map : HashMap<String, String> = HashMap::new();
-
-    let lines : i32 = file::get_lines_count(file_name.clone());
-
-    for i in 0..lines-1 {
-        if !check_null::is_int_null(i) {
-            let res : String = file::get_line(i, file_name.clone());
-            if !check_null::is_string_null(res.clone()) {
-                map.extend(set_strings(res));
-            }
-        }
-    }
-
-    map
-}
-
-pub fn set_strings(line: String) -> HashMap<String, String> {
+pub fn set_string(line: String) -> HashMap<String, String> {
     let mut line_array : Vec<String> = line.split(" ").map(|s| s.to_string()).collect();
 
     let mut strings: HashMap<String, String> = HashMap::new();
-    
-    let mut chars = line_array[3].chars();
-    chars.next_back();
-    line_array[3] = chars.as_str().to_string();
 
     if &line_array[0] != &"string".to_string() {
         return strings;
     }
+    
+    let mut chars = line_array[3].chars();
+    chars.next_back();
+    line_array[3] = chars.as_str().to_string();
     
     if !line_array[3].contains(&'"'.to_string()) {
         println!("string {}, is not an string", line_array[1]);
