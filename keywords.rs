31962cacd7d5681e2a7c0750708313b153b437c0
mjkova <78625324+mjkova@users.noreply.github.com>
2022-12-24 17:17:19 -0700

    Update keywords.rs

--- keywords.rs
+++ keywords.rs
@@ -3,21 +3,25 @@ use std::collections::HashMap;
 mod convert;
 use crate::execute;
 
-pub fn get_and_execute(line: String, hash: HashMap<String, i32>) -> bool {
+pub fn get_and_execute(line: String, hash: HashMap<String, i32>, strings: HashMap<String, String>) -> bool {
     if line.contains("print") {
-        return print_parse(line, hash);
+        return print_parse(line, hash, strings);
     }
     return false;
 }
 
 // BEGIN KEYWORD PRINT
-fn print_parse(line: String, hash: HashMap<String, i32>) -> bool {
+fn print_parse(line: String, hash: HashMap<String, i32>, strings: HashMap<String, String>) -> bool {
     let slice = convert::convert_string::find_between(line.clone(), '(', ')');
         
     let firstch = convert::convert_string::get_letter(slice.clone(), 0);
 
     if execute::is_int_variable(slice.clone().to_string(), hash.clone()) {
         print(execute::get_int(slice.clone().to_string(), hash.clone()).to_string());
+        return false;
+    }
+    if execute::is_string_variable(slice.clone().to_string(), strings.clone()) {
+        print(execute::get_string(slice.clone().to_string(), strings.clone()).to_string());
         return false;
     }
     if firstch == '"'.to_string() {
